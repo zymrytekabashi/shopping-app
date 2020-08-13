dcc1b88a6cc032afe1929d1ddff138cf350f0a61
zymryte <zymryteeek@gmail.com>
2020-08-13 13:38:02 +0200

    adding some basic bootstrap in recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is a simply a test', 'https://static01.nyt.com/images/2020/02/10/dining/onepot-cheesypasta/onepot-cheesypasta-articleLarge.jpg'),
+    new Recipe('A test Recipe', 'This is a simply a test', 'https://static01.nyt.com/images/2020/02/10/dining/onepot-cheesypasta/onepot-cheesypasta-articleLarge.jpg'),
+       
+  ];
   constructor() { }
 
   ngOnInit(): void {
